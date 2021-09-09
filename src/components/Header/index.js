0eee06ec02f8ebe0c98d1607b83866761898a741
George <gosha.kim1998@mail.ru>
2021-09-09 16:49:48 +0600

    rename button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -81,7 +81,7 @@ class Header extends Component {
                   root: classes.btn,
                 }}
               >
-                test
+                test1
               </Button>
             </Box>
           </Toolbar>
